c9a9099ac6e18f164c0d0d39052a440d62b21503
HusamAjour <husamaj278@gmail.com>
2020-09-22 22:10:29 +0300

    Add javascript

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var time = new Date;
+prompt("Hello, The time now is " + time.getHours() + ":" + time.getMinutes()) 
+
+var name = prompt("What's your name?");
+
+var username = document.getElementById("username");
+
+username.innerHTML = name;
+
+colorValue = prompt("Choose one of the following by entering its number: 1- black 2- blue 3- white")
+
+if(colorValue == 1 ){
+    document.getElementById("main").style.backgroundColor = "black";
+}
+else if(colorValue == 2 ){
+    document.getElementById("main").style.backgroundColor = "blue";
+}
+else if(colorValue == 3 ){
+    document.getElementById("main").style.backgroundColor = "white";
+}
+else{
+    alert("wrong input");
+}
