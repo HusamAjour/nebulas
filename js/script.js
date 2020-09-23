b552fb1f4887da0ff940f6f1d1edef9a396884e2
HusamAjour <husamaj278@gmail.com>
2020-09-23 11:42:06 +0300

    add functions

--- js/script.js
+++ js/script.js
@@ -1,25 +1,34 @@
-var time = new Date;
-alert("Hello, The time now is " + time.getHours() + ":" + time.getMinutes()); 
+function viewTime() {
+    var time = new Date;
+    alert("Hello, The time now is " + time.getHours() + ":" + time.getMinutes());
+}
 
 var name = " Stranger"
 
-name = prompt("What's your name?");
-
-var username = document.getElementById("username");
-
-username.innerHTML = name;
-
-colorValue = prompt("Choose one of the following by entering its number: 1- black 2- blue 3- white")
-
-if(colorValue == 1 ){
-    document.getElementById("main").style.backgroundColor = "black";
+function displayName() {
+    name = prompt("What's your name?");
+    var username = document.getElementById("username");
+    username.innerHTML = name;
 }
-else if(colorValue == 2 ){
-    document.getElementById("main").style.backgroundColor = "blue";
+
+function changeBackgroundColor() {
+    colorValue = prompt("Choose one of the following by entering its number: 1- black 2- blue 3- white")
+
+    if (colorValue == 1) {
+        document.getElementById("main").style.backgroundColor = "black";
+    }
+    else if (colorValue == 2) {
+        document.getElementById("main").style.backgroundColor = "blue";
+    }
+    else if (colorValue == 3) {
+        document.getElementById("main").style.backgroundColor = "white";
+    }
+    else {
+        alert("wrong input");
+    }
 }
-else if(colorValue == 3 ){
-    document.getElementById("main").style.backgroundColor = "white";
-}
-else{
-    alert("wrong input");
-}
+
+
+viewTime();
+displayName();
+changeBackgroundColor();
